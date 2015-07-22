4664b2053cf5baeecb1855c075335d77b7abd18a
Simon Swain <simon@simonswain.com>
2015-07-23 07:14:02 +1000

    Temp disable

--- server/public/js/scenes/coldwar.js
+++ server/public/js/scenes/coldwar.js
@@ -976,7 +976,7 @@ Scenes.coldwar = function(el, opts){
       html += '<div id="controls">';
       html += '<div id="params">';
       html += '<div class="expose expose-params expose-active">&equiv;</div>';
-      html += '<div class="title"><a href="attract">Cold War</a></div>';
+      html += '<div class="title">Cold War</div>';
       html += '<p><a href="https://twitter.com/simon_swain" target="new">@simon_swain</a></p>';
       html += '<p><a href="https://github.com/simonswain/coldwar" target="new">#coldwar</a></p>';
       html += '<p>Type <strong>?</strong> for Help</p>';
@@ -984,7 +984,7 @@ Scenes.coldwar = function(el, opts){
       html += '<div id="options"></div>';
       html += '</div>';
     } else {
-      html += '<div class="title"><a href="attract">Cold War</a></div>';
+      html += '<div class="title">Cold War</div>';
       html += '<div class="expose expose-params">&equiv;</div>';
     }
 
